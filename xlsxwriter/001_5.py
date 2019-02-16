7a5b90290e1836851c3f69826e1cb30d3b28e44c
SIEEID <sieeid8128@gmail.com>
2019-02-16 07:50:13 +0000

    Test

--- xlsxwriter/001_5.py
+++ xlsxwriter/001_5.py
@@ -14,3 +14,24 @@ student_math_score = (
 
 row = 0
 col = 0
+
+for student, score in (student_math_score):
+    worksheet.write(row, col, student)
+    worksheet.write(row, col + 1, score)
+    Pass = '=IF(B' + str(row + 1) + '>70,"Pass", "Fail")'
+    worksheet.write_formula(row, col + 2, Pass)
+    row += 1
+
+worksheet.write(row, 0, 'Sum', bold)
+worksheet.write(row, 1, '=SUM(B1:B4)', bold)
+# worksheet.write_formula(B5, '=B1 + B2 + B3 + B4')
+worksheet.write(row + 1, 0, 'Average', bold)
+worksheet.write(row + 1, 1, '=AVERAGE(B1:B4)', bold)
+# worksheet.write_formula(B6, '= (B1 + B2 + B3 + B4) / 4')
+
+worksheet.write_formula('A7', '=DATEVALUE("1-Jan-2013")')
+
+workbook.close()
+
+
+
---
+++ xlsxwriter/e001.py
@@ -0,0 +1,33 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('tutorial_1_5.xlsx')
+worksheet = workbook.add_worksheet()
+
+bold = workbook.add_format({'bold': True})
+
+columns = (
+    ['국어', '영어', '수학', '과학', '평균', '합격여부']
+)
+student_math_score = (
+    ['hojun', 95, 72, 36, 90],
+    ['eunjung', 75, 42, 55, 62],
+    ['subin', 98, 30, 61, 30],
+    ['eunbin', 67, 70, 70, 81]
+)
+
+for index, column in enumerate(columns):
+    worksheet.write(0, index + 1, column)
+
+for rowIndex, scoreData in enumerate(student_math_score):
+    row = rowIndex + 1
+    for col, inform in enumerate(scoreData):
+        worksheet.write(row, col, inform)
+    average = '=AVERAGE(B' + str(row + 1) + ':E' + str(row + 1) + ')'
+    worksheet.write(row, len(scoreData), average)
+    Pass = '=IF(F' + str(row + 1) + '>70,"Pass", "Fail")'
+    worksheet.write_formula(row, len(scoreData) + 1, Pass)
+
+workbook.close()
+
+
+
